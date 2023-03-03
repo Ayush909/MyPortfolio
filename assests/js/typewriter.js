a97f1321d0d4bbf6e4f73ab664ec32eb9377fb13
Ayush Singh Kushwaha <50234003+Ayush909@users.noreply.github.com>
2023-03-03 14:02:44 +0530

    adding resume and updating content

--- assests/js/typewriter.js
+++ assests/js/typewriter.js
@@ -1,19 +1,17 @@
-var app = document.getElementById('typing');
+var app = document.getElementById("typing");
 
 var typewriter = new Typewriter(app, {
-    loop: true,
-    delay : 1
+  loop: true,
+  delay: 1,
 });
 
-
-
-
-typewriter.typeString("<span id='first_type'>Software Engineer</span>")
-    .pauseFor(1500)
-    .typeString('<br/>')
-    .typeString("<span id='second_type'>Web Developer</span>")
-    .pauseFor(1500)
-    .typeString('<br/>')
-    .typeString("<span id='third_type'>Tech Enthusiast</span>")
-    .pauseFor(1500)
-    .start();
+typewriter
+  .typeString("<span id='first_type'>Frontend Developer</span>")
+  .pauseFor(1500)
+  .typeString("<br/>")
+  .typeString("<span id='second_type'>Javascript Expert</span>")
+  .pauseFor(1500)
+  .typeString("<br/>")
+  .typeString("<span id='third_type'>ReactJs Developer</span>")
+  .pauseFor(1500)
+  .start();
