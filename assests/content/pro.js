656e583094f7946ef17773316cf4c76ee8cccf6c
Ayush Singh Kushwaha <50234003+Ayush909@users.noreply.github.com>
2023-05-20 17:26:33 +0530

    adding product card project

--- assests/content/pro.js
+++ assests/content/pro.js
@@ -1,50 +1,55 @@
-
 const projects = [
-    
-    {       
-        title : "Instagram Clone",
-        body : "Instagram Clone using MERN Stack.",
-        img : "assests/img/insta.jpg",
-        link : "http://instagram-ayush.herokuapp.com/",
-        type : "fs"        
-    },
-    ,
-    {    
-        title : "Restaurant UI/UX",
-        body : "A Restaurant landing page made using ReactJS, HTML & CSS",
-        img : "assests/img/restaurant.png",
-        link : "https://reactui-restaurant.netlify.app/",
-        type : 'fe'    
-    },
-    {    
-        title : "Blogging CMS",
-        body : "A Blogging Content Management System using React-Admin.",
-        img : "assests/img/blog_cms.jpg",
-        link : "https://hidden-chamber-48931.herokuapp.com/",
-        type : 'be'    
-    },
-    {    
-        title : "Merchant Log",
-        body : "A place for business owners to list their business using MERN Stack.",
-        img : "assests/img/merchantlog.jpg",
-        link : "https://mern-stack-merchant.herokuapp.com/",
-        type : 'fs'    
-    },
-    {    
-        title : "Blog Vlog",
-        body : "Personal Blog using github api to convert md format to html code.",
-        img : "assests/img/blogvlog.jpg",
-        link : "https://blogvlog.netlify.app/",
-        type : 'fe'    
-    },
-    {
-        title : "JS Clock",
-        body : "Simple clock using vanilla javascript.",
-        img : "assests/img/clock.gif",
-        link : "https://codepen.io/ayush909/pen/LYQKxZG",
-        type : 'des' 
-    }
-]
+  {
+    title: "Instagram Clone",
+    body: "Instagram Clone using MERN Stack.",
+    img: "assests/img/insta.jpg",
+    link: "http://instagram-ayush.herokuapp.com/",
+    type: "fs",
+  },
+  ,
+  {
+    title: "Restaurant UI/UX",
+    body: "A Restaurant landing page made using ReactJS, HTML & CSS",
+    img: "assests/img/restaurant.png",
+    link: "https://reactui-restaurant.netlify.app/",
+    type: "fe",
+  },
+  {
+    title: "Blogging CMS",
+    body: "A Blogging Content Management System using React-Admin.",
+    img: "assests/img/blog_cms.jpg",
+    link: "https://hidden-chamber-48931.herokuapp.com/",
+    type: "be",
+  },
+  {
+    title: "Merchant Log",
+    body: "A place for business owners to list their business using MERN Stack.",
+    img: "assests/img/merchantlog.jpg",
+    link: "https://mern-stack-merchant.herokuapp.com/",
+    type: "fs",
+  },
+  {
+    title: "Blog Vlog",
+    body: "Personal Blog using github api to convert md format to html code.",
+    img: "assests/img/blogvlog.jpg",
+    link: "https://blogvlog.netlify.app/",
+    type: "fe",
+  },
+
+  {
+    title: "Product Card",
+    body: "Product Card Preview Component using HTML, CSS",
+    img: "assests/img/Product_Card.png",
+    link: "https://ayush909.github.io/Product-Preview/",
+    type: "des",
+  },
+  {
+    title: "JS Clock",
+    body: "Simple clock using vanilla javascript.",
+    img: "assests/img/clock.gif",
+    link: "https://codepen.io/ayush909/pen/LYQKxZG",
+    type: "des",
+  },
+];
 
 export default projects;
-
